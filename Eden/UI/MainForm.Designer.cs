323ec20f1e03ba0e3ee6511e050fe64738e7ba92
ナム <n4mc0de@gmail.com>
2025-04-15 12:45:18 +0700

    sửa 1 tý

--- Eden/UI/MainForm.Designer.cs
+++ Eden/UI/MainForm.Designer.cs
@@ -240,8 +240,8 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1555, 972);
             this.Controls.Add(this.guna2Panel2);
             this.Controls.Add(this.guna2Panel1);
