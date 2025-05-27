fc73ff2490cb6799ac21529e391a4b438c24bee6
ナム <n4mc0de@gmail.com>
2025-05-27 20:21:34 +0700

    Reapply "first step"
    This reverts commit 9ad0f9380b4c54b9785daa7b84f031a10b1eb35c.

--- Eden/UI/MainForm.cs
+++ Eden/UI/MainForm.cs
@@ -55,6 +55,10 @@ namespace Eden
                 buttonInfos.Add(("Nhập kho", "btnNK", "ware"));
                 buttonInfos.Add(("Nhà cung cấp", "btnNCC", "cc"));
             }
+            if (CurrentUser.Permissions?.Contains("TDQD") == true)
+            {
+                buttonInfos.Add(("Quản lý lương", "btnS", "salary"));
+            }
 
             foreach (var info in buttonInfos)
             {
@@ -177,6 +181,12 @@ namespace Eden
                                 // Xử lý Nhà cung cấp
                                 pnlMainContent.Controls.Clear();
                                 ShowForm(new NhaCungCapForm());
+                                break;
+
+                            case "btnS":
+                                // Xử lý Quản lý lương
+                                pnlMainContent.Controls.Clear();
+                                ShowForm(new SanPhamForm());
                                 break;
                         }
                     };
